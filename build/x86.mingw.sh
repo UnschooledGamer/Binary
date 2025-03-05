4e8a032934ee84fb7ac164d178aed14d784673e7
Emmanuel Lobo <76094069+UnschooledGamer@users.noreply.github.com>
2025-03-05 22:56:54 +0530

    Update x86.mingw.sh

--- build/x86.mingw.sh
+++ build/x86.mingw.sh
@@ -1,6 +1,9 @@
 # Install dependency
 pacman -S --noconfirm --needed autoconf automake libtool pkgconf mingw-w64-i686-gcc mingw-w64-i686-pkg-config make cmake mingw-w64-i686-libogg tree
 
+# export PREFIX 
+export PREFIX="$(pwd)/mingw32"
+
 # Make directory
 mkdir bin
 
@@ -8,7 +11,7 @@ mkdir bin
 cd ./libsodium
 echo "[LunaStream Builder]: Build sodium binary"
 ./autogen.sh
-./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
+./configure --host=i686-w64-mingw32 --prefix="$PREFIX" --disable-static --enable-shared &&
 make
 make install
 cp ./mingw32/bin/libsodium-26.dll ../bin/sodium-win32-x86.dll
