1590e5766d91084407a820e35d47bf4a0f179179
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-03-05 20:53:27 +0700

    add: let it make only

--- build/x64.mingw.sh
+++ build/x64.mingw.sh
@@ -3,67 +3,39 @@ pacman -S --noconfirm --needed autoconf automake libtool pkgconf mingw-w64-x86_6
 
 # Make directory
 mkdir bin
-touch ./bin/sodium.tree.x64.log
 
 # Build sodium
 cd ./libsodium
-if [ -e "./mingw64/bin/libsodium-26.dll" ]; then
-  echo "[LunaStream Builder]: Copy sodium binary"
-  cp ./mingw64/bin/libsodium-26.dll ../bin/sodium-win32-x64.dll
-else
-  echo "[LunaStream Builder]: Build sodium binary"
-  ./autogen.sh
-  ./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
-    make clean &&
-    make &&
-    make check &&
-    make install
-  cp ./mingw64/bin/libsodium-26.dll ../bin/sodium-win32-x64.dll
-fi
-tree > ./bin/sodium.tree.x64.log
+echo "[LunaStream Builder]: Build sodium binary"
+./autogen.sh
+./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
+make
+cp ./mingw64/bin/libsodium-26.dll ../bin/sodium-win32-x64.dll
 cd ..
 
 # Build opus
 cd libopus
-if [ -e "./.libs/libopus-0.dll" ]; then
-  echo "[LunaStream Builder]: Copy opus binary"
-  cp ./.libs/libopus-0.dll ../bin/opus-win32-x64.dll
-else
-  echo "[LunaStream Builder]: Build opus binary"
-  ./autogen.sh
-  ./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
-    make clean &&
-    make &&
-    make check &&
-    make install
-  cp ./.libs/libopus-0.dll ../bin/opus-win32-x64.dll
-fi
+echo "[LunaStream Builder]: Build opus binary"
+./autogen.sh
+./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
+make
+cp ./.libs/libopus-0.dll ../bin/opus-win32-x64.dll
 cd ..
 
 # Build vorbis
 cd libvorbis
-if [ -e "./lib/.libs/libvorbis-0.dll" ]; then
-  echo "[LunaStream Builder]: Copy vorbis binary"
-  cp ./lib/.libs/libvorbis-0.dll ../bin/vorbis-win32-x64.dll
-else
-  echo "[LunaStream Builder]: Build vorbis binary"
-  ./autogen.sh
-  ./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
-  make
-  cp ./lib/.libs/libvorbis-0.dll ../bin/vorbis-win32-x64.dll
-fi
+echo "[LunaStream Builder]: Build vorbis binary"
+./autogen.sh
+./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
+make
+cp ./lib/.libs/libvorbis-0.dll ../bin/vorbis-win32-x64.dll
 cd ..
 
 # Build fdk-aac
 cd libfdkaac
-if [ -e "./.libs/libfdk-aac-2.dll" ]; then
-  echo "[LunaStream Builder]: Copy fdk-aac binary"
-  cp ./.libs/libfdk-aac-2.dll ../bin/fdk-aac-win32-x64.dll
-else
-  echo "[LunaStream Builder]: Build fdk-aac binary"
-  ./autogen.sh
-  ./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
-  make
-  cp ./.libs/libfdk-aac-2.dll ../bin/fdk-aac-win32-x64.dll
-  cd ..
-fi
+echo "[LunaStream Builder]: Build fdk-aac binary"
+./autogen.sh
+./configure --host=x86_64-w64-mingw32 --prefix=/mingw64 --disable-static --enable-shared &&
+make
+cp ./.libs/libfdk-aac-2.dll ../bin/fdk-aac-win32-x64.dll
+cd ..
--- build/x86.mingw.sh
+++ build/x86.mingw.sh
@@ -2,68 +2,40 @@
 pacman -S --noconfirm --needed autoconf automake libtool pkgconf mingw-w64-i686-gcc mingw-w64-i686-pkg-config make cmake mingw-w64-i686-libogg tree
 
 # Make directory
-mkdir -p ./bin
-touch ./bin/sodium.tree.x64.log
+mkdir bin
 
 # Build sodium
 cd ./libsodium
-if [ -e "./mingw32/bin/libsodium-26.dll" ]; then
-  echo "[LunaStream Builder]: Copy sodium binary"
-  cp ./mingw32/bin/libsodium-26.dll ../bin/sodium-win32-x86.dll
-else
-  echo "[LunaStream Builder]: Build sodium binary"
-  ./autogen.sh
-  ./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
-    make clean &&
-    make &&
-    make check &&
-    make install
-  cp ./mingw32/bin/libsodium-26.dll ../bin/sodium-win32-x86.dll
-fi
-tree > ./bin/sodium.tree.x64.log
+echo "[LunaStream Builder]: Build sodium binary"
+./autogen.sh
+./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
+make
+cp ./mingw32/bin/libsodium-26.dll ../bin/sodium-win32-x86.dll
 cd ..
 
 # Build opus
 cd libopus
-if [ -e "./.libs/libopus-0.dll" ]; then
-  echo "[LunaStream Builder]: Copy opus binary"
-  cp ./.libs/libopus-0.dll ../bin/opus-win32-x86.dll
-else
-  echo "[LunaStream Builder]: Build opus binary"
-  ./autogen.sh
-  ./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
-    make clean &&
-    make &&
-    make check &&
-    make install
-  cp ./.libs/libopus-0.dll ../bin/opus-win32-x86.dll
-fi
+echo "[LunaStream Builder]: Build opus binary"
+./autogen.sh
+./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
+make
+cp ./.libs/libopus-0.dll ../bin/opus-win32-x86.dll
 cd ..
 
 # Build vorbis
 cd libvorbis
-if [ -e "./lib/.libs/libvorbis-0.dll" ]; then
-  echo "[LunaStream Builder]: Copy vorbis binary"
-  cp ./lib/.libs/libvorbis-0.dll ../bin/vorbis-win32-x86.dll
-else
-  echo "[LunaStream Builder]: Build vorbis binary"
-  ./autogen.sh
-  ./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
-  make
-  cp ./lib/.libs/libvorbis-0.dll ../bin/vorbis-win32-x86.dll
-fi
+echo "[LunaStream Builder]: Build vorbis binary"
+./autogen.sh
+./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
+make
+cp ./lib/.libs/libvorbis-0.dll ../bin/vorbis-win32-x86.dll
 cd ..
 
 # Build fdk-aac
 cd libfdkaac
-if [ -e "./.libs/libfdk-aac-2.dll" ]; then
-  echo "[LunaStream Builder]: Copy fdk-aac binary"
-  cp ./.libs/libfdk-aac-2.dll ../bin/fdk-aac-win32-x86.dll
-else
-  echo "[LunaStream Builder]: Build fdk-aac binary"
-  ./autogen.sh
-  ./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
-  make
-  cp ./.libs/libfdk-aac-2.dll ../bin/fdk-aac-win32-x86.dll
-fi
+echo "[LunaStream Builder]: Build fdk-aac binary"
+./autogen.sh
+./configure --host=i686-w64-mingw32 --prefix=/mingw32 --disable-static --enable-shared &&
+make
+cp ./.libs/libfdk-aac-2.dll ../bin/fdk-aac-win32-x86.dll
 cd ..
