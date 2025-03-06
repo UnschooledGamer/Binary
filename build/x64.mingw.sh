d183fa5401337cbf075cdf09be79ee4eb5c8ada4
Emmanuel Lobo <76094069+UnschooledGamer@users.noreply.github.com>
2025-03-06 07:34:31 +0530

    Update x64.mingw.sh

--- build/x64.mingw.sh
+++ build/x64.mingw.sh
@@ -14,7 +14,9 @@ echo "[LunaStream Builder]: Build sodium binary"
 ./configure --host=x86_64-w64-mingw32 --prefix="$PREFIX" --disable-static --enable-shared &&
 make
 make install
-cp ./mingw64/bin/libsodium-26.dll ../bin/sodium-win32-x64.dll
+cp ./mingw64/bin/libsodium-26.dll ../bin/sodium-win32-x64.dll 
+echo "$PREFIX" 
+tree "$PREFIX" 
 tree .
 cd ..
 
